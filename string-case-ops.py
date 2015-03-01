2f3c0be6de98e14355e09c16d72af83e636244db
Suresh Kumar <sureshdsk91@gmail.com>
2015-03-01 20:09:33 +0530

    string ops script

---
+++ string-case-ops.py
@@ -0,0 +1,9 @@
+# String conversion in Python
+# @IdiotInside_
+str = "Idiot Inside"
+print ("captilize() ",str.capitalize())
+print ("casefold() ",str.casefold())  #python 3.3
+print ("swapcase() ",str.swapcase())
+print ("title() ",str.title())
+print ("lower() ",str.lower())
+print ("upper() ",str.upper())
