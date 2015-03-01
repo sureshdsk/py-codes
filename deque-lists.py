89bd64525511fb5e9b1a83ccfd1e12c22352cb2e
Suresh Kumar <sureshdsk91@gmail.com>
2015-03-01 20:47:04 +0530

    fifo lifo lists

---
+++ deque-lists.py
@@ -0,0 +1,35 @@
+# Tutorial: http://www.idiotinside.com/2015/03/01/use-python-lists-as-fifo-lifo-queues-using-deque-collections
+
+from collections import deque
+
+dq = deque(['b','c','d'])
+print dq
+
+# adding an element to the right of the queue
+dq.append('e')
+print dq
+
+# adding an element to the left of the queue
+dq.appendleft('a')
+print dq
+
+# iterate over deque's elements
+for elt in dq:
+    print(elt)
+
+# pop out an element at from the right of the queue
+dq.pop()
+print dq
+
+# pop out an element at from the right of the queue
+dq.popleft()
+print dq
+
+# print as list
+print list(dq)
+
+# reversed list
+print list(reversed(dq))
+
+# empty the deque element
+dq.clear()
---
+++ lists-stack.py
@@ -0,0 +1,20 @@
+# Tutorial: http://www.idiotinside.com/2015/03/01/use-python-lists-as-fifo-lifo-queues-using-deque-collections
+
+stack = ["a", "b", "c"]
+
+# add an element to the end of the list
+stack.append("e")
+stack.append("f")
+print stack
+
+# pop operation
+stack.pop()
+print stack
+
+# pop operation
+stack.pop()
+print stack
+
+# push operation
+stack.append("d")
+print stack
