4943ca4857c8279f92e5ee6cf0c1e954944a16b5
Suresh Kumar <sureshdsk91@gmail.com>
2015-03-01 20:06:07 +0530

    adding arithmetic ops script

---
+++ arithmetic-ops.py
@@ -0,0 +1,27 @@
+# Arithmetic Operations in Python
+# @IdiotInside_
+a = 200
+b = 10
+x = 11
+y = 3
+
+c = a + b
+print ("Addition ==> a+b = ", c)
+
+c = a - b
+print ("Subtraction ==> a-b = ", c )
+
+c = a * b
+print ("Multiplication ==> a*b = ", c)
+
+c = a / b
+print ("Division ==> a/b = ", c )
+
+c = a // b
+print ("Floor Division ==> a//b = ", c )
+
+z = x % y
+print ("Modulo ==> a%b = ", z)
+
+z = x**y
+print ("Exponent ==> x**y = ", z)
